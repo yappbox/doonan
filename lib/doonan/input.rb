8a09d426eed4833ed37a46c17bd9dabc7b9abd1b
Luke Melia <luke@lukemelia.com>
2011-12-22 13:45:07 -0500

    Add image methods to scope

--- lib/doonan/input.rb
+++ lib/doonan/input.rb
@@ -8,6 +8,12 @@ module Doonan
       settings_path = File.join(path, 'settings.json')
       settings = JSON.parse(File.read(settings_path))
       @scope = OpenStruct.new(settings)
+      
+      Dir["#{images_path}/*"].each do |image_path|
+        image_slug = File.basename(image_path).chomp(File.extname(image_path))
+        @scope.send("has_image_#{image_slug}?=".to_sym, true)
+        @scope.send("image_path_#{image_slug}=".to_sym, File.basename(image_path))
+      end
     end
 
     attr_reader :scope
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -4,11 +4,11 @@ require 'doonan'
 describe "example output" do
   before :all do
     templates_path = File.expand_path('../example_templates', __FILE__)
-    input_path = File.expand_path('../example_input', __FILE__)
+    @input_path = File.expand_path('../example_input', __FILE__)
     @out_path = File.expand_path('../../tmp/example-output', __FILE__)
     FileUtils.rm_rf(@out_path)
     doonan = Doonan::Generator.new(templates_path)
-    doonan.generate(input_path, @out_path)
+    doonan.generate(@input_path, @out_path)
   end
   
   it "should output the hello world color" do
@@ -27,5 +27,24 @@ describe "example output" do
     parser = CssParser::Parser.new
     parser.load_file!('foo.css', @out_path)
     parser.find_by_selector('.icon2').should == []
+  end
+  
+  describe "Input" do
+    it "should include json properties in the scope" do
+      input = Doonan::Input.new(@input_path)
+      input.scope.hello_world_color.should == 'blue'
+    end
+    
+    it "should include info about image presence in the scope" do
+      input = Doonan::Input.new(@input_path)
+      input.scope.has_image_icon_1?.should == true
+      input.scope.has_image_icon_2?.should == nil
+    end
+    
+    it "should include info about image paths in the scope" do
+      input = Doonan::Input.new(@input_path)
+      input.scope.image_path_icon_1.should == 'icon_1.jpg'
+      input.scope.image_path_icon_2.should == nil
+    end
   end
 end
