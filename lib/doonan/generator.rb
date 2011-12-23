a1f8277e2609eb1c4337cec64fc2d6f34206dfd5
Kris Selden <kris.selden@gmail.com>
2011-12-22 16:35:29 -0800

    fix sass config, files not still not ending up in right spot after pass so bar is not found

--- lib/doonan/generator.rb
+++ lib/doonan/generator.rb
@@ -2,45 +2,35 @@ module Doonan
   class Generator
     def initialize(templates_path)
       @templates_path = templates_path
-      self.class.add_compass_load_paths_once
-      Compass.configuration.images_path = 'images'
     end
-    
+
     def generate(input_path, output_path, extensions)
       input = Doonan::Input.new(input_path)
       FileUtils.mkdir_p(output_path)
       staging_output_path = File.join(output_path, "tmp-staging")
       FileUtils.rm_rf(staging_output_path)
       FileUtils.cp_r(@templates_path, staging_output_path)
-      FileUtils.cd(input_path) do
-        extensions.each_with_index do |extension|
-          pass_templates = templates(staging_output_path, extension)
-          pass_templates.each do |template|
-            output = template.render(input.scope)
-            render_output_path = File.join(staging_output_path, template.output_filename)
-            File.open(render_output_path, 'w') do |f|
-              f.puts output
-            end
-            FileUtils.rm(template.template_path)
+      Compass.configuration.images_path = File.join(input_path, 'images')
+      load_paths = [staging_output_path]
+      extensions.each_with_index do |extension|
+        pass_templates = templates(staging_output_path, extension, load_paths)
+        pass_templates.each do |template|
+          output = template.render(input.scope)
+          render_output_path = File.join(staging_output_path, template.output_filename)
+          File.open(render_output_path, 'w') do |f|
+            f.puts output
           end
+          FileUtils.rm(template.template_path)
         end
       end
       FileUtils.cp_r("#{staging_output_path}/.", output_path)
       # FileUtils.rm_rf(staging_output_path)
     end
-    
-    def templates(templates_path, extension)
+
+    def templates(templates_path, extension, load_paths)
       Dir["#{templates_path}/**/*.#{extension}"].map do |template_path|
-        Doonan::Template.new(template_path)
+        Doonan::Template.new(template_path, load_paths)
       end
-    end
-  
-  private
-    def self.add_compass_load_paths_once
-      unless @__added_compass_load_paths_once
-        Sass::Engine::DEFAULT_OPTIONS[:load_paths].concat(Compass.sass_engine_options[:load_paths])
-      end
-      @__added_compass_load_paths_once = true
     end
   end
 end
--- lib/doonan/template.rb
+++ lib/doonan/template.rb
@@ -5,24 +5,26 @@ require 'compass'
 
 module Doonan
   class Template
-    def initialize(template_path)
+    def initialize(template_path, load_paths)
       @template_path = template_path
-      @extension = File.extname(template_path)
-      @output_filename = File.basename(template_path).chomp(@extension)
-      @template_class = Tilt[@extension]
-      if @template_class == Tilt::SassTemplate
-        debugger
-        @template_class.options[:load_path]
+      extension = File.extname(template_path)
+      @output_filename = File.basename(template_path).chomp(extension)
+      template_class = Tilt[extension]
+      options = {}
+      if template_class <= Tilt::SassTemplate
+        load_paths += Compass.sass_engine_options[:load_paths]
+        options[:load_paths] = load_paths
+        options[:style] = :expanded
+        options[:line_comments] = true
       end
+      @template = template_class.new(template_path, 1, options)
     end
-    
+
     attr_reader :template_path
     attr_reader :output_filename
-    
+
     def render(scope)
-      template_source = File.read(@template_path)
-      t = @template_class.new { |template| template_source }
-      t.render(scope)
+      @template.render(scope)
     end
   end
 end
